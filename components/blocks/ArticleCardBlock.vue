620855534ff886588caa653258d1b4378b8567b3
Yohei Kokubo <yhei.register@gmail.com>
2021-11-03 09:00:39 +0900

    Using fetch in page components、fetchでデータ取得・リンク表示まで  - Developing the Application #3

---
+++ components/blocks/ArticleCardBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt-link :to="{ name: 'username-article', params: { username: article.user.username, article: article.id } }">
+    hoge
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <Tutorial/>
+  <div class="page-wrapper">
+    <div class="article-cards-wrapper">
+      <article-card-block
+        v-for="(article, i) in articles"
+        :key="article.id"
+        :article="article"
+        class="article-card-block"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import ArticleCardBlock from '@/components/blocks/ArticleCardBlock'
+
+export default {
+  components: {
+    ArticleCardBlock
+  },
+  data() {
+    return {
+      currentPage: 1,
+      articles: []
+    }
+  },
+  async fetch() {
+    const articles = await fetch(
+      `https://dev.to/api/articles?tag=nuxt&state=rising&page=${this.currentPage}`
+    ).then(res => res.json())
+    this.articles = this.articles.concat(articles)
+    console.log(this.articles)
+  },
+}
 </script>
