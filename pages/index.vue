da37a41e0b7170158877b6da21f98c4027f2eb19
Yohei Kokubo <yhei.register@gmail.com>
2021-11-04 12:50:40 +0900

    Applying placeholders with $fetchState。fetch中のplaceholder的なブロックを配置 - Developing the Application #3

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="page-wrapper">
-    <div class="article-cards-wrapper">
-      <article-card-block
-        v-for="(article, i) in articles"
-        :key="article.id"
-        v-observe-visibility="
-          i === articles.length - 1 ? lazyLoadArticles : false
-        "
-        :article="article"
-        class="article-card-block"
-      />
-    </div>
+    <template v-if="$fetchState.pending">
+      <div class="article-cards-wrapper">
+        <content-placeholders
+          v-for="p in 30" :key="p"
+          rounded
+          class="article-card-block"
+        >
+          <content-placeholders-img />
+          <content-placeholders-text :lines="3" />
+        </content-placeholders>
+      </div>
+    </template>
+    <template v-else-if="$fetchState.error">
+      <p>
+        {{ $fetchState.error.message }}
+      </p>
+    </template>
+    <template v-else>
+      <div class="article-cards-wrapper">
+        <article-card-block
+          v-for="(article, i) in articles"
+          :key="article.id"
+          v-observe-visibility="
+            i === articles.length - 1 ? lazyLoadArticles : false
+          "
+          :article="article"
+          class="article-card-block"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
