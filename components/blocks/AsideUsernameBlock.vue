2205e614397971b42d62471132e3549a5b862874
Yohei Kokubo <yhei.register@gmail.com>
2021-11-09 12:55:53 +0900

    記事一覧のサイドバー - Developing the Application #3

--- components/blocks/AsideUsernameBlock.vue
+++ components/blocks/AsideUsernameBlock.vue
@@ -1,15 +1,51 @@
 <template>
   <aside class="aside-username-block">
     <template v-if="user">
-      <div>username: {{ user.name }}</div>
-      <div v-if="user.twitter_username">twitter_username: @{{ user.twitter_username }}</div>
       <img :src="user.profile_image" :alt="user.name" width="90" height="90">
+      <div class="mt-3">
+        <nuxt-link :to="{
+          name: 'username',
+          params: { username: user.username }
+        }">{{ user.name }}</nuxt-link>
+      </div>
+      <div class="username-block__sns-links mt-3">
+        <a
+          v-if="user.twitter_username"
+          :href="`https://twitter.com/${user.twitter_username}`"
+          target="_blank"
+        >
+          <twitter-icon />
+        </a>
+        <a
+          v-if="user.github_username"
+          :href="`https://github.com/${user.github_username}`"
+          target="_blank"
+        >
+          <github-icon />
+        </a>
+        <a
+          v-if="user.website_url"
+          :href="`${user.website_url}`"
+          target="_blank"
+        >
+          <external-link-icon />
+        </a>
+      </div>
     </template>
   </aside>
 </template>
 
 <script>
+import TwitterIcon from '~/assets/icons/twitter.svg?inline'
+import GithubIcon from '~/assets/icons/github.svg?inline'
+import ExternalLinkIcon from '~/assets/icons/external-link.svg?inline'
+
 export default {
+  components: {
+    TwitterIcon,
+    GithubIcon,
+    ExternalLinkIcon
+  },
   data() {
     return {
       user: {}
@@ -27,3 +63,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.aside-username-block {
+  box-shadow: 2px 2px 8px $shadowColor;
+  padding: 2rem;
+}
+</style>
